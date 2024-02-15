49f02dc31fe840e2629dae5c8370031761970c14
UnaiFernandez <fernandezunai027@gmail.com>
2024-02-15 22:14:07 +0100

    Prueba de comunicacion con el modulo ESP01

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,6 +41,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -51,6 +52,7 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -58,6 +60,7 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 void led_toggle(uint8_t led);
+uint8_t rx_buff[80];
 /* USER CODE END 0 */
 
 /**
@@ -89,22 +92,35 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
+  HAL_Delay(3000);
 
+  uint8_t command[] = "AT\r\n";
+  HAL_UART_Transmit(&huart1, command, sizeof(command), 10);
+  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+
+
+  HAL_UART_Receive_IT(&huart1, rx_buff, 10);
+
+
+  HAL_UART_Transmit(&huart2, "Hola buenas!", 12, 10);
+  HAL_UART_Transmit(&huart2, rx_buff, sizeof(rx_buff), 10);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
+
+    while (1)
   {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    uint8_t Test[] = "Hello World !!!\r\n"; //Data to send
-    HAL_UART_Transmit(&huart2,Test,sizeof(Test),10);// Sending in normal mode
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    //uint8_t Test[] = "Hello World !!!\r\n"; //Data to send
+    //HAL_UART_Transmit(&huart2,Test,sizeof(Test),10);// Sending in normal mode
+    //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
     
-    HAL_Delay(1000);
+    //HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
@@ -148,6 +164,39 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
 }
 
 /**
@@ -212,7 +261,9 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+  HAL_UART_Receive_IT(&huart, rx_buff, 10);
+}
 /* USER CODE END 4 */
 
 /**
